116202468ba798542d163978c51d97428a0991bf
James Peres <james.peres.s@gmail.com>
2018-05-31 18:43:50 +0100

    add validate post code

--- postcode.py
+++ postcode.py
@@ -2,6 +2,13 @@ import re
 
 
 def check_format(code):
-     second_part = code.split(' ')[1].upper()
+     second_part = code.split(' ')[-1].upper()
      if re.search('\d{1}[A-Z]{2}', second_part):
          return code.upper()
+
+def validate_code(code):
+    regex_validate = '^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) [0-9][A-Za-z]{2})$'
+
+    if re.match(regex_validate, str(check_format(code))):
+        return 'Post Code valid!'
+    return 'Error in code'
--- test_postcode.py
+++ test_postcode.py
@@ -14,10 +14,20 @@ class PostCodeTest(unittest.TestCase):
         self.assertEqual(check_format('QWE 1QW'), 'QWE 1QW')
         self.assertEqual(check_format('qwe 9qw'), 'QWE 9QW')
 
+    def test_validate_post_code_missing_value(self):
+        self.assertEqual(validate_code('QWE 1W'), 'Error in code')
 
+    def test_validate_post_code_missing_value(self):
+        self.assertEqual(validate_code('Q 1W'), 'Error in code')
 
+    def test_validate_post_code_missing_value_first(self):
+        self.assertEqual(validate_code('1W'), 'Error in code')
 
+    def test_validate_post_code_missing_value_first(self):
+        self.assertEqual(validate_code('1W'), 'Error in code')
 
+    def test_validate_post_code_correct(self):
+        self.assertEqual(validate_code('CV4 8UW'), 'Post Code valid!')
 
 
 if __name__ == '__main__':
