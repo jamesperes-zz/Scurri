8bfe4ac1da9a237ba1db9b0b27b10437a73e0764
James Peres <james.peres.s@gmail.com>
2018-05-31 18:11:30 +0100

    change function for return code or false

--- test_postcode.py
+++ test_postcode.py
@@ -5,15 +5,14 @@ from postcode import *
 class PostCodeTest(unittest.TestCase):
 
     def test_tree_last_digits_missing_one(self):
-        self.assertEqual(check_format('QWE QW'), 'Not a valid PostCode in UK')
+        self.assertFalse(check_format('QWE QW'))
 
     def test_tree_last_digits_sequence_wrong(self):
-        self.assertEqual(check_format('QWE QW1'), 'Not a valid PostCode in UK')
+        self.assertFalse(check_format('QWE QW1'))
 
     def test_tree_last_digits_correct(self):
         self.assertEqual(check_format('QWE 1QW'), 'QWE 1QW')
         self.assertEqual(check_format('qwe 9qw'), 'QWE 9QW')
-
 
 
 
