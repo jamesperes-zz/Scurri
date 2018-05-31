55cd788778aa15eff59be42fbbce1f418145f4eb
James Peres <james.peres.s@gmail.com>
2018-05-31 20:29:56 +0100

    change function for format code

--- postcode.py
+++ postcode.py
@@ -1,14 +1,16 @@
 import re
 
 
-def check_format(code):
-     second_part = code.split(' ')[-1].upper()
-     if re.search('\d{1}[A-Z]{2}', second_part):
-         return code.upper()
+def format_code(code):
+    code_to_check = code.upper().strip()
+    first_part = code_to_check[:-3].strip()
+    second_part = code_to_check[-3:]
+    if re.search('\d{1}[A-Z]{2}', second_part):
+        return "{} {}".format(first_part, second_part)
 
 def validate_code(code):
     regex_validate = '^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) [0-9][A-Za-z]{2})$'
 
-    if re.match(regex_validate, str(check_format(code))):
+    if re.match(regex_validate, str(format_code(code))):
         return 'Post Code valid!'
     return 'Error in code'
--- test_postcode.py
+++ test_postcode.py
@@ -5,14 +5,17 @@ from postcode import *
 class PostCodeTest(unittest.TestCase):
 
     def test_tree_last_digits_missing_one(self):
-        self.assertFalse(check_format('QWE QW'))
+        self.assertFalse(format_code('QWE QW'))
 
     def test_tree_last_digits_sequence_wrong(self):
-        self.assertFalse(check_format('QWE QW1'))
+        self.assertFalse(format_code('QWE QW1'))
+
+    def test_digits_sequence_with_number_tohether(self):
+        self.assertFalse(format_code('QWEQW1'))
 
     def test_tree_last_digits_correct(self):
-        self.assertEqual(check_format('QWE 1QW'), 'QWE 1QW')
-        self.assertEqual(check_format('qwe 9qw'), 'QWE 9QW')
+        self.assertEqual(format_code('QWE 1QW'), 'QWE 1QW')
+        self.assertEqual(format_code('qwe 9qw'), 'QWE 9QW')
 
     def test_validate_post_code_missing_value(self):
         self.assertEqual(validate_code('QWE 1W'), 'Error in code')
@@ -23,12 +26,14 @@ class PostCodeTest(unittest.TestCase):
     def test_validate_post_code_missing_value_first(self):
         self.assertEqual(validate_code('1W'), 'Error in code')
 
-    def test_validate_post_code_missing_value_first(self):
-        self.assertEqual(validate_code('1W'), 'Error in code')
-
     def test_validate_post_code_correct(self):
         self.assertEqual(validate_code('CV4 8UW'), 'Post Code valid!')
 
+    def test_validate_post_code_correct_but_together(self):
+        self.assertEqual(validate_code('CV48UW'), 'Post Code valid!')
+
+    def test_validate_post_code_empty_value(self):
+        self.assertEqual(validate_code(''), 'Error in code')
 
 if __name__ == '__main__':
     unittest.main() 
