0decea5b4213c130b44b75d7c51e5e0661c84e1a
James Peres <james.peres.s@gmail.com>
2018-05-31 22:00:30 +0100

    refactor base and tests

--- test_postcode.py
+++ test_postcode.py
@@ -4,36 +4,30 @@ from postcode import *
 
 class PostCodeTest(unittest.TestCase):
 
-    def test_tree_last_digits_missing_one(self):
-        self.assertFalse(format_code('QWE QW'))
+    def test_format_missing_one_last_digit(self):
+        self.assertEqual(format_code('QWE QW'), 'QWE  QW')
 
-    def test_tree_last_digits_sequence_wrong(self):
-        self.assertFalse(format_code('QWE QW1'))
-
-    def test_digits_sequence_with_number_tohether(self):
-        self.assertFalse(format_code('QWEQW1'))
-
-    def test_tree_last_digits_correct(self):
+    def test_format_six_digits(self):
         self.assertEqual(format_code('QWE 1QW'), 'QWE 1QW')
         self.assertEqual(format_code('qwe 9qw'), 'QWE 9QW')
 
     def test_validate_post_code_missing_value(self):
-        self.assertEqual(validate_code('QWE 1W'), 'Error in code')
+        self.assertFalse(validate_code('QWE 1W'))
 
-    def test_validate_post_code_missing_value(self):
-        self.assertEqual(validate_code('Q 1W'), 'Error in code')
+    def test_validate_post_code_wrong_value(self):
+        self.assertFalse(validate_code('Q 1W'))
 
     def test_validate_post_code_missing_value_first(self):
-        self.assertEqual(validate_code('1W'), 'Error in code')
+        self.assertFalse(validate_code('1WW'))
 
     def test_validate_post_code_correct(self):
-        self.assertEqual(validate_code('CV4 8UW'), 'Post Code valid!')
+        self.assertTrue(validate_code('CV4 8UW'))
 
-    def test_validate_post_code_correct_but_together(self):
-        self.assertEqual(validate_code('CV48UW'), 'Post Code valid!')
+    def test_validate_post_code_correct_with_values_together(self):
+        self.assertTrue(validate_code('CV48UW'))
 
     def test_validate_post_code_empty_value(self):
-        self.assertEqual(validate_code(''), 'Error in code')
+        self.assertFalse(validate_code(''))
 
 if __name__ == '__main__':
     unittest.main() 
