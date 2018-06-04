620e362a56709f39a27a8b495701fe066c84f1e6
James Peres <james.peres.s@gmail.com>
2018-06-04 23:25:40 +0100

    refactor logic

--- postalcode.py
+++ postalcode.py
@@ -21,8 +21,8 @@ def format_code(code):
 
 def validate_code(code):
     """ Validate UK Postal Code with regex available in
-    https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/488478/Bulk_Data_Transfer_-_additional_validation_valid_from_12_November_2015.pdf
-
+    https://en.wikipedia.org/wiki/Postcodes_in_the_United_Kingdom#Formatting
+    
     Keywords Arguments:
     code - string
 
@@ -30,8 +30,45 @@ def validate_code(code):
     type - Boolean
     """
 
-    regex_validate = '^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) [0-9][A-Za-z]{2})$'
 
-    if re.match(regex_validate, format_code(code)):
+    regex_validate = '(GIR 0AA|[A-PR-UWYZ]([0-9]{1,2}|([A-HK-Y][0-9]|[A-HK-Y][0-9]([0-9]|[ABEHMNPRV-Y]))|[0-9][A-HJKPS-UW]) [0-9][ABD-HJLNP-UW-Z]{2})'
+
+    """Explaining Regex
+    GIR 0AA = last domestic postcode with a fully alphabetical outward code
+    [A-PR-UWYZ] =  The letters QVX are not used in the first position.
+    [A-HK-Y] = The letters IJZ are not used in the second position.
+    [A-HJKPS-UW] = The only letters to appear in the third position are 
+                   ABCDEFGHJKPSTUW when the structure starts with A9A. 
+    [ABEHMNPRV-Y] = The only letters to appear in the fourth position are 
+                    ABEHMNPRVWXY when the structure starts with AA9A.
+    [ABD-HJLNP-UW-Z]{2} = The final two letters do not use the letters CIKMOV
+    """
+
+    single_digits = ['BR','FY','HA','HD','HG','HR','HS','HX','JE','LD','SM',\
+                     'SR','WC','WN','ZE']
+    double_digits = ['AB', 'LL', 'SO']
+    digit_zero = ['BL', 'BS', 'CM', 'CR', 'FY', 'HA', 'PR', 'SL', 'SS']
+
+    code = format_code(code)
+    if code[:2] in double_digits:
+        if re.match('([A-Z]{2}[0-9]{2} [0-9]{1}[BD-HJLNP-UW-Z]{2}$)', code):
+            return True
+        else:
+            return False
+
+    if code[:2] in single_digits:
+        if re.match('([A-Z]{2}[0-9]{1} [0-9]{1}[BD-HJLNP-UW-Z]{2}$)', code):
+            return True
+        else:
+            return False
+
+    if code[:2] in digit_zero:
+        if re.match('([A-Z]{2}[0]{1} [0-9]{1}[BD-HJLNP-UW-Z]{2}$)', code):
+            return True
+        else:
+            return False
+
+    if re.match(regex_validate, code):
         return True
-    return False
+    else:
+       return False
--- test_postalcode.py
+++ test_postalcode.py
@@ -29,5 +29,71 @@ class PostalCodeTest(unittest.TestCase):
     def test_validate_post_code_empty_value(self):
         self.assertFalse(validate_code(''))
 
+    def test_validate_post_code_invalid_missing_one_number(self):
+        self.assertFalse(validate_code('AB1 1AA'))
+
+    def test_validate_post_code_valid(self):
+        self.assertTrue(validate_code('AB12 3XY'))
+        self.assertFalse(validate_code('AB12 1AA'))
+
+    def test_validate_post_code_invalid_with_QVX_first_possition(self):
+        self.assertFalse(validate_code('WQA 0AX'))
+        self.assertFalse(validate_code('WVA 0AX'))
+        self.assertFalse(validate_code('WXA 0AX'))
+
+    def test_validate_post_code_invalid_with_IJZ_second_possition(self):
+        self.assertFalse(validate_code('AJ1 1AA'))
+        self.assertFalse(validate_code('BJ1 1AA'))
+        self.assertFalse(validate_code('CZ1 1AA'))
+
+    def test_validate_post_code_valid_with_third_possition(self):
+        valid = 'ABCDEFGHJKPSTUW'
+        for i in valid:
+            self.assertTrue(validate_code(f"W1{i} 0AX"))
+
+    def test_validate_post_code_invalid_with_third_possition(self):
+        invalid = 'ILMNOQRVXYZ'
+        for i in invalid:
+            self.assertFalse(validate_code(f"W1{i} 0AX"))
+
+    def test_validate_post_code_valid_with_fourth_possition(self):
+        valid = 'ABEHMNPRVWXY'
+        for i in valid:
+            self.assertTrue(validate_code(f"EC1{i} 1BB"))
+
+    def test_validate_post_code_invalid_with_fourth_possition(self):
+        invalid = 'CDFGIJKLOQSTUZ'
+        for i in invalid:
+            self.assertFalse(validate_code(f"EC2{i} 1BB"))
+
+    def test_validate_post_code_invalid_final_letters(self):
+        invalid = 'CIKMOV'
+        for i in invalid:
+            self.assertFalse(validate_code(f"CR2 6{i}H"))
+
+        for i in invalid:
+            self.assertFalse(validate_code(f"CR2 6X{i}"))
+
+    def test_validate_post_code_valid_with_single_digit_district(self):
+        valid = ['BR','FY','HA','HD','HG','HR','HS','HX','JE','LD','SM','SR','WC','WN','ZE']
+
+        for i in valid:
+            self.assertTrue(validate_code(f"{i}1 1BB"))
+
+    def test_validate_post_code_valid_with_double_digit_district(self):
+        valid = ['AB', 'LL', 'SO']
+
+        for i in valid:
+            self.assertTrue(validate_code(f"{i}12 1BB"))
+
+
+    def test_validate_post_code_valid_with_double_digit_district_zero(self):
+        valid = ['BL', 'BS', 'CM', 'CR', 'FY', 'HA', 'PR', 'SL', 'SS'] 
+
+        for i in valid:
+            self.assertTrue(validate_code(f"{i}0 1BB"))
+
+
+
 if __name__ == '__main__':
     unittest.main() 
